0fa1a1bd4860ececf3cbfd9ae529a21e24b0946e
JulianVe <julian@veasy.org>
2025-05-17 22:50:15 +0100

    Update api_stack.py add cors

--- deployment/stacks/api_stack.py
+++ deployment/stacks/api_stack.py
@@ -9,9 +9,29 @@ class APIStack(Stack):
         self.api = apigateway.RestApi(
             self, "APIGateway",
             rest_api_name="veasy_org_test_arq_message_api",
-            description="Handles message retrieval."
+            description="Handles message retrieval.",
+            default_cors_preflight_options={
+                "allow_origins": apigateway.Cors.ALL_ORIGINS,
+                "allow_methods": apigateway.Cors.ALL_METHODS,
+                "allow_headers": ["Content-Type", "X-Amz-Date", "Authorization", "X-Api-Key", "X-Amz-Security-Token"]
+            }
         )
 
         # Create API resource (/message)
         message_resource = self.api.root.add_resource("message")
-        message_resource.add_method("GET", apigateway.LambdaIntegration(lambda_function))
+
+        # Enable CORS for the /message resource
+        message_resource.add_method(
+            "GET",
+            apigateway.LambdaIntegration(lambda_function),
+            method_responses=[
+                apigateway.MethodResponse(
+                    status_code="200",
+                    response_parameters={
+                        "method.response.header.Access-Control-Allow-Origin": True,
+                        "method.response.header.Access-Control-Allow-Methods": True,
+                        "method.response.header.Access-Control-Allow-Headers": True
+                    }
+                )
+            ]
+        )
