93c4ce7d792d548027ffcdee2289e21bd69e6dd8
julianve <julian@veasy.org>
2025-05-17 20:11:03 +0100

    rename stack id

--- deployment/stacks/dynamodb_stack.py
+++ deployment/stacks/dynamodb_stack.py
@@ -9,7 +9,7 @@ class DynamoDBStack(Stack):
         super().__init__(scope, id, **kwargs)
 
         self.table = dynamodb.Table(
-            self, "DynamoDBTable",
+            self, "DynamoDbTable",
             table_name=TABLE_NAME, 
             partition_key=dynamodb.Attribute(name="id", type=dynamodb.AttributeType.STRING),
             billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST
