28446e5e4bd83e3d1ffc82563ccf147420637dff
julianve <julian@veasy.org>
2025-05-17 16:09:44 +0100

    fix for broken dependency

--- deployment/stacks/lambda_stack.py
+++ deployment/stacks/lambda_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import Stack
 from constructs import Construct
 import aws_cdk.aws_lambda as _lambda
-from .dynamodb_stack import TABLE_NAME
+#from .dynamodb_stack import TABLE_NAME
 
 class LambdaStack(Stack):
     def __init__(self, scope: Construct, id: str, dynamodb_table, **kwargs):
@@ -12,7 +12,8 @@ class LambdaStack(Stack):
             runtime=_lambda.Runtime.PYTHON_3_9,
             handler="lambda_function.handler",
             code=_lambda.Code.from_asset("../backend"),
-            environment={TABLE_NAME: dynamodb_table.table_name}
+            #environment={TABLE_NAME: dynamodb_table.table_name}
+            environment={}
         )
 
         dynamodb_table.grant_read_data(self.lambda_function)
