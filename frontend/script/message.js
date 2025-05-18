85516a6e41a2a93d9c1710b461c843556433a03a
julianve <julian@veasy.org>
2025-05-18 08:42:01 +0100

    fix for cors errors

--- frontend/script/message.js
+++ frontend/script/message.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", async () => {
     try {
-        const response = await fetch("https://mvetfj0ueb.execute-api.eu-west-2.amazonaws.com/prod/message");
-        const data = await response.json();
+        const response = await fetch("https://mvetfj0ueb.execute-api.eu-west-2.amazonaws.com/prod/message", {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+        });
 
+        const data = await response.json();
         document.getElementById("message").textContent = `The saved string is ${data.text}`;
     } catch (error) {
         console.error("Error fetching message:", error);
