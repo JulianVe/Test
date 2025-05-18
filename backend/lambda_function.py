397a9844d471039134470c25dda50a2b7dc122d6
julianve <julian@veasy.org>
2025-05-18 09:30:27 +0100

    api gateway is proxy to lambda so lambda need to return cors headers

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -9,6 +9,10 @@ def handler(event, context):
 
     return {
         "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization"
+        },
         "body": json.dumps({"text": message_provider.get_message()})
     }
