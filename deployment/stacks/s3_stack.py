762c55636059a57ecde7fca7d6be869836631620
julianve <julian@veasy.org>
2025-05-24 11:01:57 +0100

    delete and recreate the bucket

--- deployment/stacks/s3_stack.py
+++ deployment/stacks/s3_stack.py
@@ -5,31 +5,24 @@ import aws_cdk.aws_s3 as s3
 BUCKET_NAME = "veasy.org.test.arq"
 
 class S3Stack(Stack):
-    def __init__(self, scope: Construct, id: str, import_existing: bool = True, **kwargs):
+    def __init__(self, scope: Construct, id: str, **kwargs):
         super().__init__(scope, id, **kwargs)
 
-        if import_existing:
-            # Import the bucket if it already exists.
-            self.bucket = s3.Bucket.from_bucket_name(
-                self, "FrontendBucket",
-                bucket_name=BUCKET_NAME
-            )
-        else:
-            # Create a new bucket.
-            self.bucket = s3.Bucket(
-                self, "FrontendBucket",
-                bucket_name=BUCKET_NAME,
-                block_public_access=s3.BlockPublicAccess.BLOCK_ACLS,
-                public_read_access=True,
-                website_index_document="index.html"
-            )
-            
-            # Add CORS configuration. 
-            self.bucket.add_cors_rule(
-                allowed_methods=[s3.HttpMethods.GET, s3.HttpMethods.POST],
-                allowed_origins=["*"],
-                allowed_headers=["*"]
-            )
+        # Create a new bucket.
+        self.bucket = s3.Bucket(
+            self, "FrontendBucket",
+            bucket_name=BUCKET_NAME,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ACLS,
+            public_read_access=True,
+            website_index_document="index.html"
+        )
+
+        # Add CORS configuration. 
+        self.bucket.add_cors_rule(
+            allowed_methods=[s3.HttpMethods.GET, s3.HttpMethods.POST],
+            allowed_origins=["*"],
+            allowed_headers=["*"]
+        )
 
         # Output the website URL of the bucket.
         CfnOutput(
